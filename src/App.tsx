d216ac5ea7d9a049410c0beeec28c0b1a9a8ad29
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2023-08-15 20:14:37 +0300

    Navbar demo ready

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={<LandingPage />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,11 @@ import "../styles/Navbar.css";
 const Navbar = () => {
     return (
         <div className="navbar">
-            <span className="logo"><Link className="link" to="/">SinunShow</Link></span>
+            <span className="navButtonBorderLeft"><Link className="link" to="/experience">Experience</Link></span>
+            <span className="navButton"><Link className="link" to="/projects">Projects</Link></span>
+            <span className="logo"><Link className="linkHome" to="/">GudokVlad</Link></span>
+            <span className="navButton"><Link className="link" to="/hobbies">Activities and hobbies</Link></span>
+            <span className="navButtonBorderRight"><Link className="link" to="/about">About</Link></span>
         </div>
     )
 }
