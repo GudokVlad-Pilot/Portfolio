0e5d212d3f36712e131ac2ed620d3462d4676a4c
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2023-08-17 14:06:18 +0300

    small modifications

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,10 +1,11 @@
 import "../styles/InfoCard.css"
+import Stars from "../components/img/Stars.jpg"
 
 function InfoCard() {
     return (
         <div className='infoCard'>
             <span className="title">Title</span>
-            {/* <img src={} alt="" className="img" /> */}
+            <img src={Stars} alt="" className="img" />
             <p className="desc">Description</p>
             <button className="cardButton">Read More</button>
         </div>
